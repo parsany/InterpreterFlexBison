02c6c842cbc7d322c113c5b10ce8303854f57ee9
quantinity <113546376+parsany@users.noreply.github.com>
2025-09-07 21:09:53 +0330

    Create lang.py

---
+++ lang.py
@@ -0,0 +1,19 @@
+// lang.py
+
+int main() {
+    show(11);
+    let a = 10;
+    let b = 20;
+    let c = a + b;
+    show(c); // Should show 30
+    let d = a+b+c*2;
+    show(d);
+    let x = 4;
+    show(2*2*2*2*2);
+    show(x*2);
+
+    let result = 10 * (x + 5);
+    show(result);
+    let k = vorood();
+    show(k*2+1);
+}
